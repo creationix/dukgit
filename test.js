68ff130f0be41a5c172d04eae212f53e4a6a7acb
Tim Caswell <tim@creationix.com>
2015-09-18 16:36:47 -0500

    Merge in git-sha1 to core

--- test.js
+++ test.js
@@ -6,6 +6,11 @@ var fs = require('./fs.js')();
 var storage = require('./storage.js')(fs);
 p(storage);
 
+var bodec = require('./bodec.js');
+var git = require('./core.js')(bodec);
+var modes = git.modes;
+p(git);
+
 Duktape.Thread.resume(new Duktape.Thread(function () {
   storage.write("test/path/file", "Hello World\n");
   storage.put("test/file2", "Good\n");
@@ -20,18 +25,15 @@ Duktape.Thread.resume(new Duktape.Thread(function () {
   while ((entry = it())) { p(entry); }
   storage.remove("test/path/file");
   storage.remove("test/file2");
+
+  var data = git.frame({
+    type: "tree",
+    body: [
+      { name: "index.html", mode: modes.blob, hash: "80b38a9171a9c61ac590e3867f56322027240e7e" },
+    ]
+  });
+  p(data);
+  p(git.deframe(data, true));
+
+  p(git.sha1(""));
 }));
-
-var bodec = require('./bodec.js');
-var git = require('./core.js')(bodec);
-var modes = git.modes;
-p(git);
-
-var data = git.frame({
-  type: "tree",
-  body: [
-    { name: "index.html", mode: modes.blob, hash: "80b38a9171a9c61ac590e3867f56322027240e7e" },
-  ]
-});
-p(data);
-p(git.deframe(data, true));
